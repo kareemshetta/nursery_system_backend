029a4548cc14f41c5693b9cd03d8334d77571750
kareem shetta <62408980+kareemshetta@users.noreply.github.com>
2023-07-18 04:47:10 +0300

    Update childModel.js

--- model/childModel.js
+++ model/childModel.js
@@ -7,11 +7,7 @@
  * 
  */
 
-const { request } = require("express");
 const mongoose = require("mongoose");
-const mongooseShema = mongoose.Schema;
-const autoIncrement = require("@alec016/mongoose-autoincrement");
-autoIncrement.initialize(mongoose.connection);
 
 const address = new mongoose.Schema(
   {
@@ -28,5 +24,5 @@ const schema = new mongoose.Schema({
   level: String,
 });
 
-schema.plugin(autoIncrement.plugin, "childs");
 mongoose.model("childs", schema);
+
